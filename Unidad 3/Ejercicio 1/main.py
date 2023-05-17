a831ea0165af92802f8c400d8040fef57aab7b70
Carlos Catalan <103954507+Kuadc@users.noreply.github.com>
2023-05-17 18:16:04 -0300

    Create main.py

---
+++ Unidad 3/Ejercicio 1/main.py
@@ -0,0 +1,16 @@
+from ManejadorFacultad import ListaFacultad
+
+from ClaseMenu import Menu
+
+if __name__ == '__main__':
+    listafacu = ListaFacultad()
+    listafacu.AbrirArchivo()
+    #print(listafacu)
+    unmenu = Menu()
+    while True:
+        print('\n-------Menu--------')
+        print("1) - Mostrar Datos de la Facultad.")
+        print("2) - Dado el nombre de la carrera, mostrar codigo, nombre y localidad de facultad")
+        print("3) - Finalizar")
+        opcion= input("\nIngrese la opcion Deseada: ")
+        unmenu.opcion(opcion,listafacu)
