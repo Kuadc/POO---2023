a92105fdf8893be52983f8d1d5cba47af1f7be34
Carlos Catalan <103954507+Kuadc@users.noreply.github.com>
2023-05-22 18:36:19 -0300

    Create ClaseInterface.py

---
+++ Unidad 3/Ejercicio 5/ClaseInterface.py
@@ -0,0 +1,13 @@
+from zope.interface import Interface
+
+
+class EjercicioInterface(Interface):
+
+    def insertarelemento(elemento,posicion):
+        pass
+
+    def agregarelemento(elemento):
+        pass
+
+    def mostrarElemento(posicion):
+        pass
