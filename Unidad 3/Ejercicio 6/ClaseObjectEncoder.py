d1ce0271ea923d81a16782eb08f34e9775ffa55b
Carlos Catalan <103954507+Kuadc@users.noreply.github.com>
2023-05-28 13:24:57 -0300

    Update ClaseObjectEncoder.py

--- Unidad 3/Ejercicio 6/ClaseObjectEncoder.py
+++ Unidad 3/Ejercicio 6/ClaseObjectEncoder.py
@@ -8,22 +8,22 @@ from ClaseVehiculoNuevo import VehiculoNuevo
 from ClaseVehiculoUsado import VehiculoUsado
 class ObjectEncoder(object):
     def decodificarDiccionario(self, d):
-        if '__class__' not in d: #si la la clave "__class__" no esa presente en el diccionario (por lo tanto no se puede decodificar el diccionario porque no contiene información sobre la clase)
+        if '__class__' not in d: 
          return d
         else:
-            class_name=d['__class__'] # en contenido de la clave "__class__" del diccionario "d" se asigna a class_name
-            class_=eval(class_name) # se utiliza la funcion eval, para evaluar el nombre de la clase y obtener la referencia a la clase, se asigna a a la variable.
-            if class_name=='Lista': #si la el nombre de la clase es igual a el manejador "lista". se decodifica el contenido
-                vehiculo=d['Vehiculo'] # el cotenido de la clave "vehiculo" se asigna a vehiculo
-                unvehiculo = vehiculo[0] # se obtiene el primer elementto de la lista puntos y se guarda en "dpunto"
-                manejadorlista=class_()  # se crea una instancia con el nombre manejador ( que es una lista)
+            class_name=d['__class__']
+            class_=eval(class_name) 
+            if class_name=='Lista': 
+                vehiculo=d['Vehiculo']
+                unvehiculo = vehiculo[0] 
+                manejadorlista=class_()  
                 for i in range(len(vehiculo)):
-                    unvehiculo = vehiculo[i] #el primer elemento de la lista puntos que contiene los objetos/instancia de la clase punto
-                    class_name = unvehiculo.pop('__class__') #se obtiene el nobre de la clase utilziando "pop", que elimina y obtiene el valor de la clavwe "__class__"
-                    class_ = eval(class_name) #evalua nuevamente el bnombre de la clase y se guiarda en class_
-                    atributos = unvehiculo['__atributos__'] #se obtiene todos los atributos que tiene el objeto punto y se guarda como  un dicionaro
-                    unveh= class_(**atributos) #se crea una instancia de la clase punto (en este caso) y se le pasan por parametos los atributos.
-                    manejadorlista.AgregarElemento(unveh) # se agregar los puntos a la lista.
+                    unvehiculo = vehiculo[i] 
+                    class_name = unvehiculo.pop('__class__') 
+                    class_ = eval(class_name) 
+                    atributos = unvehiculo['__atributos__'] 
+                    unveh= class_(**atributos) 
+                    manejadorlista.AgregarElemento(unveh)
                 return manejadorlista
 
     def guardarJSONArchivo(self, diccionario, archivo):
