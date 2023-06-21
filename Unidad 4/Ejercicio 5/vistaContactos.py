94bd76a09dce3c4e43e0509b3c811fe52b81e71c
Carlos Catalan <103954507+Kuadc@users.noreply.github.com>
2023-06-21 12:13:23 -0300

    Update vistaContactos.py

--- Unidad 4/Ejercicio 5/vistaContactos.py
+++ Unidad 4/Ejercicio 5/vistaContactos.py
@@ -20,38 +20,49 @@ class ContactList(tk.Frame):
 
 
 class ContactForm(tk.LabelFrame):
-    fields = ("Titulo", "resumen", "Lenguaje", "Fecha", "Generos")
+    fields = ("Titulo", "Lenguaje", "Fecha", "Generos", "Resumen")
     def __init__(self, master, **kwargs):
         super().__init__(master, text="Pelicula", padx=10, pady=10, **kwargs)
         self.frame = tk.Frame(self)
         self.entries = list(map(self.crearCampo, enumerate(self.fields)))
         self.frame.pack()
+
     def crearCampo(self, field):
         position, text = field
         label = tk.Label(self.frame, text=text)
-        entry = tk.Entry(self.frame, width=25)
+        if text == "Resumen":
+            entry = tk.Text(self.frame, width=40, height=10, font=('Calibri',10))  # Usar tk.Text para el campo "Resumen"
+        else:
+            entry = tk.Entry(self.frame, width=30)
         label.grid(row=position, column=0, pady=5)
         entry.grid(row=position, column=1, pady=5)
         return entry
+
     def mostrarEstadoContactoEnFormulario(self, pelicula):
-        # a partir de un contacto, obtiene el estado
-        # y establece en los valores en el formulario de entrada
-        values = (pelicula.get_titulo(), pelicula.get_resumen(),
-                  pelicula.get_lenguaje(), pelicula.get_fecha(), pelicula.get_generos())
+        values = (
+            pelicula.get_titulo(), pelicula.get_generos(),
+            pelicula.get_lenguaje(), pelicula.get_fecha(), pelicula.get_resumen()
+        )
         for entry, value in zip(self.entries, values):
-            entry.delete(0, tk.END)
-            entry.insert(0, value)
+            if isinstance(entry, tk.Text):  # Verificar si el campo es una instancia de tk.Text
+                entry.delete("1.0", tk.END)  # Borrar el contenido existente en el campo de texto
+                entry.insert("1.0", value)  # Insertar el valor en el campo de texto
+            else:
+                entry.delete(0, tk.END)
+                entry.insert(0, value)
+
 
     def limpiar(self):
         for entry in self.entries:
             entry.delete(0, tk.END)
+        self.text_field.delete(0, tk.END)
 
 
 class ContactsView(tk.Tk):
     def __init__(self):
         super().__init__()
         self.title("Lista de peliculas")
-        self.list = ContactList(self, height=15)
+        self.list = ContactList(self, height=20, width=35)
         self.form = UpdateContactForm(self)
         self.list.pack(side=tk.LEFT, padx=10, pady=10)
         self.form.pack(padx=10, pady=10)
@@ -70,11 +81,3 @@ class ContactsView(tk.Tk):
 class UpdateContactForm(ContactForm):
     def __init__(self, master, **kwargs):
         super().__init__(master, **kwargs)
-        self.btn_save = tk.Button(self, text="Guardar")
-        self.btn_delete = tk.Button(self, text="Borrar")
-        self.btn_save.pack(side=tk.RIGHT, ipadx=5, padx=5, pady=5)
-        self.btn_delete.pack(side=tk.RIGHT, ipadx=5, padx=5, pady=5)
-    def bind_save(self, callback):
-        self.btn_save.config(command=callback)
-    def bind_delete(self, callback):
-        self.btn_delete.config(command=callback)
